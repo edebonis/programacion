e956de3ac83b751f4c6be2f956a800d023109cf3
Esteban De Bonis <edebonis@sagradoalcal.edu.ar>
2023-09-18 14:05:32 -0300

    Ultimos cambios para compartir con Brenda

--- api2.py
+++ api2.py
@@ -1,13 +1,11 @@
-import requests
-import json
-import pprint
+import openai
 
-#pp = pprint.PrettyPrinter(depth=8)
-for i in range(1,10):
-    datos = requests.get("https://script.google.com/macros/s/AKfycbwIQi_DScVfwbNndXE3i_DKC_AiSIx07_6xPzgJlWvDkLrH_ETRIG3Td5We2h3ooIaDzw/exec?m={}".format(i))
-    dj = json.loads(datos.text)
-    print(dj['docente'],dj['nombre'])
-#print(type(dj['data'][0]['rank']))
-#pp.pprint(dj['data'][0]['rank'])
-#pp.pprint(dj)
+openai.api_key = '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'
 
+response = openai.Completion.create(
+    engine='text-davinci-003',
+    prompt='¿Cómo estás?',
+    max_tokens=50
+)
+
+print(response.choices[0].text)
